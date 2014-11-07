0a45ba6f90b185e51d2cb8b5b9f600d7a24ff7f2
Rich Collins <richcollins@gmail.com>
2014-11-07 15:14:52 -0800

    Basic task creation

--- bin/app.js
+++ bin/app.js
@@ -2,9 +2,14 @@ var ae = require("../lib/asana_export");
 var aei = require("../lib/asana_export_importer");
 
 //*
+
+//aei.Team.replacePostCreateWithFind(); //TODO
+//aei.Project.replacePostCreateWithFind(); //TODO
+
 var app = aei.App.shared();
 app.importer().setExport(ae.AsanaExport.clone().setPath(process.env.EXPORT_FILE_PATH));
 app.setApiClient(aei.asana.Client.basicAuth(process.env.ASANA_API_KEY));
+//app.setApiClient(aei.MockApiClient.clone()); //TODO
 app.start().resolve(function(err){
 	if (err) {
 		console.log(err.stack || err);
