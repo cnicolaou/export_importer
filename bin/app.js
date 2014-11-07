9bcfb6f9ccfa17f0aea890fddff1166a6caf6ddb
Rich Collins <richcollins@gmail.com>
2014-11-06 16:57:08 -0800

    Extract Export into a separate lib.  Import teams and projects.

--- bin/app.js
+++ bin/app.js
@@ -1,20 +1,13 @@
-var ideal = require("ideal");
-var asana = require("asana");
-
+var ae = require("../lib/asana_export");
 var aei = require("../lib/asana_export_importer");
 
 //*
 var app = aei.App.shared();
-app.importer().setAsanaApiKey(process.env.ASANA_API_KEY);
-app.importer().setImportFilePath(process.env.IMPORT_FILE_PATH);
-
-var futureStart = function() {
-	app.start();
-}.future();
-
-futureStart().resolve(function(err){
+app.importer().setExport(ae.AsanaExport.clone().setPath(process.env.EXPORT_FILE_PATH));
+app.setApiClient(aei.asana.Client.basicAuth(process.env.ASANA_API_KEY));
+app.start().resolve(function(err){
 	if (err) {
-		console.log(err);
+		console.log(err.stack);
 	}
 });
 
