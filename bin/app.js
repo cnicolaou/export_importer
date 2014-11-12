c9ce286528f9dcfadaa672c7832e725a615f830d
Rich Collins <richcollins@gmail.com>
2014-11-11 20:25:40 -0800

    import users, add users to projects

--- bin/app.js
+++ bin/app.js
@@ -2,13 +2,6 @@ var ae = require("../lib/asana_export");
 var aei = require("../lib/asana_export_importer");
 
 
-/*
-aei.Team.__replaceCreateWithFind(); //TODO
-aei.Project.__replaceCreateWithFind(); //TODO
-aei.Task.__replaceCreateWithFind(); //TODO
-aei.Task.__replaceSetParentWithNoop(); //TODO
-//*/
-
 var app = aei.App.shared();
 app.importer().setExport(ae.AsanaExport.clone().setPath(process.env.EXPORT_FILE_PATH));
 app.setApiClient(aei.asana.Client.basicAuth(process.env.ASANA_API_KEY));
