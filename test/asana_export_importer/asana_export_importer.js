00e2b4b79e284d4a24f459848d4c8cb3c76577b2
Tom Robinson <tlrobinson@gmail.com>
2014-12-03 02:52:00 -0800

    Add concurrency/batch-size options. Fix AsanaClientRetry and tests.

--- test/asana_export_importer/asana_export_importer.js
+++ test/asana_export_importer/asana_export_importer.js
@@ -10,7 +10,7 @@ describe("asana_export_importer", function() {
 		it("should parse with the minimal set of options, with defaults", function() {
 			process.argv = ["node", "asana_export_importer",
 				"--api-key=key",
-				"--organization=1234",
+				"--organization=0",
 				"path"
 			];
 
@@ -18,7 +18,7 @@ describe("asana_export_importer", function() {
 
 			expect(options.path).to.equal("path");
 			expect(options.module).to.equal("asana_export");
-			expect(options.organization).to.equal(1234);
+			expect(options.organization).to.equal(0);
 			expect(options.apiKey).to.equal("key");
 			expect(options.apiEndpoint).to.equal(aei.asana.Dispatcher.ROOT_URL);
 			expect(options.attachmentsPath).to.equal("db/attachments.json");
@@ -27,20 +27,24 @@ describe("asana_export_importer", function() {
 			expect(options.retryDelay).to.equal(500);
 			expect(options.retryBackoff).to.equal(2);
 			expect(options.resumable).to.equal(true);
+			expect(options.concurrency).to.equal(1000);
+			expect(options.batchSize).to.equal(100);
 		});
 
 		it("should parse all options correctly", function() {
 			process.argv = ["node", "asana_export_importer",
 				"--api-key=key",
-				"--organization=1234",
+				"--organization=1111",
 				"--importer=something",
 				"--api-endpoint=http://example.com/",
 				"--attachments=db/attachments1.json",
 				"--databases=db1",
-				"--retries=1",
-				"--retry-delay=1000",
-				"--retry-backoff=1",
+				"--retries=2222",
+				"--retry-delay=3333",
+				"--retry-backoff=4444",
 				"--resumable=false",
+				"--concurrency=5555",
+				"--batch-size=6666",
 				"path"
 			];
 
@@ -48,15 +52,17 @@ describe("asana_export_importer", function() {
 
 			expect(options.path).to.equal("path");
 			expect(options.module).to.equal("something");
-			expect(options.organization).to.equal(1234);
+			expect(options.organization).to.equal(1111);
 			expect(options.apiKey).to.equal("key");
 			expect(options.apiEndpoint).to.equal("http://example.com/");
 			expect(options.attachmentsPath).to.equal("db/attachments1.json");
 			expect(options.databasesPath).to.equal("db1");
-			expect(options.retries).to.equal(1);
-			expect(options.retryDelay).to.equal(1000);
-			expect(options.retryBackoff).to.equal(1);
+			expect(options.retries).to.equal(2222);
+			expect(options.retryDelay).to.equal(3333);
+			expect(options.retryBackoff).to.equal(4444);
 			expect(options.resumable).to.equal(false);
+			expect(options.concurrency).to.equal(5555);
+			expect(options.batchSize).to.equal(6666);
 		});
 	});
 
@@ -64,19 +70,21 @@ describe("asana_export_importer", function() {
 		it("should initialize app with correct options", function() {
 			process.argv = ["node", "asana_export_importer",
 				"--api-key=key",
-				"--organization=1234",
+				"--organization=1111",
 				"--api-endpoint=http://example.com/",
 				"--attachments=db/attachments1.json",
 				"--databases=db1",
 				"--retries=0",
 				"--resumable=false",
+				"--concurrency=5555",
+				"--batch-size=6666",
 				"path"
 			];
 
 			var app = asana_export_importer.initApp(asana_export_importer.parseOptions());
 
 			expect(app.importer().export().path()).to.equal("path");
-			expect(app.importer().organizationId()).to.equal(1234);
+			expect(app.importer().organizationId()).to.equal(1111);
 			expect(app.apiClient().dispatcher.authValue.user).to.equal("key");
 			expect(aei.asana.Dispatcher.ROOT_URL).to.equal("http://example.com/");
 			expect(app.attachmentsPath()).to.equal("db/attachments1.json");
@@ -84,6 +92,9 @@ describe("asana_export_importer", function() {
 
 			expect(app.clientCache()).to.equal(null);
 			expect(app.clientRetry()).to.equal(null);
+
+			expect(app.importer().concurrency()).to.equal(5555);
+			expect(app.importer().export().batchSize()).to.equal(6666);
 		});
 	});
 
@@ -91,11 +102,11 @@ describe("asana_export_importer", function() {
 		it("should set the cache and retry middleware parameters", function() {
 			process.argv = ["node", "asana_export_importer",
 				"--api-key=key",
-				"--organization=1234",
+				"--organization=1111",
 				"--databases=db1",
-				"--retries=10",
-				"--retry-delay=1000",
-				"--retry-backoff=1",
+				"--retries=2222",
+				"--retry-delay=3333",
+				"--retry-backoff=4444",
 				"--resumable=true",
 				"path"
 			];
@@ -106,9 +117,9 @@ describe("asana_export_importer", function() {
 			expect(app.clientCache().dbPath()).to.equal("db1/cache.sqlite");
 
 			expect(app.clientRetry()).to.not.equal(null);
-			expect(app.clientRetry().retries()).to.equal(10);
-			expect(app.clientRetry().backoff()).to.equal(1);
-			expect(app.clientRetry().delay()).to.equal(1000);
+			expect(app.clientRetry().retries()).to.equal(2222);
+			expect(app.clientRetry().delay()).to.equal(3333);
+			expect(app.clientRetry().backoff()).to.equal(4444);
 		});
 	});
 });
